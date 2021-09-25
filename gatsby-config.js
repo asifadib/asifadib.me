75440f95244fa4b2c541be901759cbe67ac6961c
Asif Adib <asifadib780@gmail.com>
2021-09-25 23:16:54 +0600

    added gatsby theme

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+const config = require('./src/config');
+
+module.exports = {
+  plugins: [
+    {
+      resolve: '@sh4hids/gatsby-theme-open-sourcerer',
+      options: {
+        ...config,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'gallery',
+        path: `${__dirname}/contents/gallery`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `galleryImages`,
+        path: `${__dirname}/contents/gallery/images`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-plugin-google-fonts-with-attributes`,
+      options: {
+        fonts: [
+          `Fira Sans:400,400i,500,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+        attributes: {
+          rel: 'stylesheet preload prefetch',
+          onLoad: "this.onload=null;this.rel='stylesheet'",
+          as: 'style',
+        },
+      },
+    },
+    `gatsby-plugin-advanced-sitemap`,
+  ],
+};
---
+++ src/@sh4hids/gatsby-theme-open-sourcerer/components/Footer.js
@@ -0,0 +1,85 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import styled from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
+import {
+  Box,
+  Container,
+  Text,
+  SocialLinks,
+} from '@sh4hids/gatsby-theme-open-sourcerer/src/components';
+
+const formatLinks = (links = {}) => {
+  const formattedLinks = [];
+
+  Object.keys(links).forEach((key) => {
+    formattedLinks.push({
+      site: key,
+      url: links[key],
+    });
+  });
+
+  return formattedLinks;
+};
+
+const Wrapper = styled(Box)`
+  background-color: var(--color-bg-1);
+  box-shadow: ${({ theme }) => theme.elevations[0]};
+`;
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author {
+            bio
+            fullName
+            links {
+              facebook
+              github
+              instagram
+              linkedin
+              twitter
+            }
+          }
+          footer {
+            title
+            description
+            copyright
+          }
+        }
+      }
+    }
+  `);
+
+  const author = data.site.siteMetadata.author || {};
+  const footer = data.site.siteMetadata.footer || {};
+  const socialLinks = formatLinks(author.links);
+
+  return (
+    <Wrapper>
+      <Container py={[3, 3, 5]}>
+        <Box display="flex" flexWrap="wrap">
+          <Box width={[1, 1, 1 / 2]} p={3}>
+            <Text fontSize="h6" fontWeight="bold" lineHeight="heading">
+              {footer.title}
+            </Text>
+            <Text variant="raw" html={footer.description} />
+          </Box>
+          <Box width={[1, 1, 1 / 2]} p={3}>
+            <Text fontSize="h6" fontWeight="bold" lineHeight="heading">
+              Social Links
+            </Text>
+            <SocialLinks links={socialLinks} />
+          </Box>
+          <Box width={1} p={3}>
+            <Text>{footer.copyright}</Text>
+          </Box>
+        </Box>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Footer;
